f2ee410a03f03fbcb5c527d9e441b4b2d3294eb2
Federico Cendali <federicocendali@gmail.com>
2022-12-28 15:18:44 -0300

    Se agrega programa en js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let alumnos = 3;
+
+alert("BIENVENIDO AL CURSO, TIENE " + alumnos + " ALUMNOS PARA EVALUAR");
+
+for (let i = 0; i < alumnos; i++) {
+    let nombreAlumno = prompt("INGRESE EL NOMBRE Y/Ó APELLIDO DEL ALUMNO:");
+    while (nombreAlumno === "") { //tambien devolver este mensaje si ingresa un numero, solo aceptar texto
+        nombreAlumno = prompt("Ups! Debe ingresar algún dato. INGRESE EL NOMBRE Y/Ó APELLIDO DEL ALUMNO:");
+    }
+    let totalDeNotas = 0;
+    let contador = 0;
+    let termino = false;
+    while(!termino) {
+        let nuevaNota = parseInt(prompt("INGRESE LA/S NOTA/S DEL ALUMNO (notas desde 1 hasta 10), Ó INGRESE '0' PARA FINALIZAR"));
+        while (nuevaNota > 10) { //solo permitir numeros, sin espacios
+            nuevaNota = parseInt(prompt("Ups! Debe ingresar una nota desde 1 hasta 10. INGRESE LA/S NOTA/S DEL ALUMNO (notas desde 1 hasta 10), Ó INGRESE '0' PARA FINALIZAR"));
+        }
+        if(nuevaNota === 0) {
+            termino = true;
+            if (totalDeNotas != 0) {
+                alert("EL PROMEDIO DE NOTAS PARA " + nombreAlumno + " ES: " + (totalDeNotas / contador));
+            } else {
+                alert("EL ALUMNO " + nombreAlumno + " NO TIENE NINGUNA NOTA");
+            }
+        } else {
+            totalDeNotas = totalDeNotas + nuevaNota;
+            contador++
+        }
+    }
+}
