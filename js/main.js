9714b985238f59c013f54394a071db7e6759425d
Federico Cendali <federicocendali@gmail.com>
2023-01-20 04:39:26 -0300

    Se resuelve errores de links http

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
 const galeria = [
     {
     persona: "Buddha - बुद्धा",
-    frase:
-        "En la confrontación entre el arrollo y la roca, el arrollo siempre ganará, no por la fuerza, sino por la persistencia.",
-    foto: "https://cdn.britannica.com/61/218561-050-47A98FB5/seated-Buddha-bronze-sculpture.jpg",
+    frase: "En la confrontación entre el arrollo y la roca, el arrollo siempre ganará, no por la fuerza, sino por la persistencia.",
+    foto: "./assets/img/main/buddha.jpg",
     },
     {
     persona: "Khalil Gibran - جبران خليل جبران بن ميخائل بن سعد",
     frase: "El silencio del envidioso está lleno de ruidos.",
-    foto: "https://trabalibros.com/rs/2632/4886d523-1977-4fa1-ab24-df1b609c897c/ed7/filename/gibran-jalil-gibran-trabalibros.jpg",
+    foto: "./assets/img/main/gibran.jpg",
     },
     {
     persona: "Confucio - 孔子",
     frase: "Todo tiene belleza pero no todo el mundo la puede ver.",
-    foto: "http://3.bp.blogspot.com/-VlsuMSoivLU/VeMlD-LymUI/AAAAAAABKPU/Q8PYwsFbqxg/s1600/confucio.jpeg",
+    foto: "./assets/img/main/confucio.jpg",
     },
     {
     persona: "Lev Nikoláievich Tolstói - Лев Николаевич Толстой",
-    frase:
-        "Mi felicidad consiste en que sé apreciar lo que tengo y no deseo con exceso lo que no tengo.",
-    foto: "http://malba.s3-website-sa-east-1.amazonaws.com/wp-content/uploads/2014/09/tolstoi-05.jpg",
+    frase: "Mi felicidad consiste en que sé apreciar lo que tengo y no deseo con exceso lo que no tengo.",
+    foto: "./assets/img/main/tolstoi.jpg",
     },
     {
     persona: "Platón - Πλάτων",
-    frase:
-        "El más importante y principal negocio público es la buena educación de la juventud.",
-    foto: "https://s-media-cache-ak0.pinimg.com/236x/ee/c4/f3/eec4f3420f7024c58f1b44de233d8ecd.jpg",
+    frase: "El más importante y principal negocio público es la buena educación de la juventud.",
+    foto: "./assets/img/main/platon.jpg",
     },
     {
     persona: "Henrik Ibsen - hɛnɾɪk ˈjoːhɑn ˈɪpsən",
     frase: "Si dudas de ti mismo, estás vencido de antemano.",
-    foto: "https://www.alohacriticon.com/wp-content/uploads/2003/08/henrik-ibsen.jpg",
+    foto: "./assets/img/main/ibsen.jpg",
     },
 ];
 
@@ -43,9 +40,7 @@ console.log(galeria[indice].persona);
 console.log(galeria[indice].frase); */
 
 document.write("<div>");
-document.write(`
-    <img src=${galeria[indice].foto} alt=${galeria[indice].persona} width=200>
-`);
+document.write(`<img src="${galeria[indice].foto}" alt="${galeria[indice].persona}" width="200">`);
 document.write("<div>");
 document.write(`<p>${galeria[indice].persona}</p>`);
 document.write(`<p>${galeria[indice].frase}</p>`);
